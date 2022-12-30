c150d62ed04d1911239b79bfd7189061d43ab828
Juliana Assis <juliana.assisoliveira@gmail.com>
2022-12-30 18:06:03 +0000

    final text and positioning

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,6 @@ import React from "react";
 import randomColor from 'randomcolor/randomColor';
 
 //Note to professor: I chose to do all the main in App.js because I didn't think it was useful to create an individual component due to the simplicity of the website and it was easier for me since I don't have a lot of experience with class components and due to the help I found online
-
 class App extends React.Component {
   state = {
     columns: 0,
@@ -32,7 +31,7 @@ class App extends React.Component {
 
     anime({
       targets: ".grid-element",
-      backgroundColor: "#fff",
+      backgroundColor: "#C69FF2",
       duration: 0,
       easing: "linear"
     });
@@ -50,17 +49,16 @@ class App extends React.Component {
     return (
         <div id="root">
           <div id="intro">
-            <p className="info mb-1 mt-2 fw-bold main">JULIANA ASSIS</p>
-            <p className="info pt-1 mb-4 fw-light main">user interface & experience designer</p>
-            <p className="info pt-1 mb-4 fw-light">A 23-year-old who mainly does UI/UX and graphic design with a bit of front-end on the side. My motto is always trying to stick to simplicity and not overcomplicating while still working on new and sometimes crazy things. A walking paradox.</p>
+            <p className="info mb-1 mt-2 fw-light main small">JULIANA ASSIS</p>
+            <p className="info pt-1 mb-4 fw-bold main">User Interface &<br /> Experience Designer</p>
+            <p className="info pt-1 mb-1 fw-light">23-year-old human who mainly does UI/UX and graphic design with a bit of front-end on the side. The motto is always trying to stick to simplicity and not overcomplicating while still working on new and sometimes crazy things. A walking paradox.</p>
             <p className="info pt-1 mb-1 fw-light">You can stalk me on LinkedIn, check some of my work on Behance and get to know me on Instagram.</p>
-            <p className="info pt-1 mb-4 fw-light">You can also have some fun with the background of this website. Enjoy :) </p>
-
-            <p className="info pt-1 mb-1">Narrator: There he goes. One of God’s own prototypes. A high-powered mutant of some kind never even considered for mass production. Too weird to live and too rare to die.</p>
-            <p className="info pt-1 mb-0">Fear and Loathing in Las Vegas (1998)</p>
-
+            <p className="info pt-1 mb-4 fw-light">You can also have some fun with the background of this website. <span className="fw-bold">Enjoy ಇ( ꈍᴗꈍ)ಇ</span></p>
+            <div className="quote">
+              <p className="info pt-1 mb-1 fw-lighter fst-italic"><span className="fst-normal">Narrator:</span> There he goes. One of God’s own prototypes. A high-powered mutant of some kind never even considered for mass production. Too weird to live and too rare to die.</p>
+              <p className="info text-end fw-lighter">Fear and Loathing in Las Vegas (Final scene), 1998</p>
+            </div>
           </div>
-
           <div id="grid">
             {[...Array(total)].map((x, i) => (
                 <div className="grid-element" id={`${i}`} onClick={(i) => this.colorWave(i)}/>
